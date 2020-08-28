02dc0dd18184134a7d4a98ebe9be2be3127f2b87
danielmac03-curso <dani-mac@hotmail.es>
2020-08-28 18:17:47 +0200

    Subir ejercicio 01 y 02

---
+++ T23-Ejercicio_01/src/main/java/es/http/service/T23Ejercicio01Application.java
@@ -0,0 +1,13 @@
+package es.http.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T23Ejercicio01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T23Ejercicio01Application.class, args);
+	}
+
+}
---
+++ T23-Ejercicio_02/src/main/java/es/http/service/T23Ejercicio02Application.java
@@ -0,0 +1,13 @@
+package es.http.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T23Ejercicio02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T23Ejercicio02Application.class, args);
+	}
+
+}
